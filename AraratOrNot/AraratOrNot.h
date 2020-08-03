b0d4f4d013d8d4c59e550b1c39b09a7e45c99464
Sevak Soghoyan <sevak@Admins-MacBook-Pro.local>
2020-08-03 15:52:00 +0400

    Initial project setup

---
+++ AraratOrNot/AraratOrNot.h
@@ -0,0 +1,19 @@
+//
+//  AraratOrNot.h
+//  AraratOrNot
+//
+//  Created by Sevak Soghoyan on 8/3/20.
+//  Copyright © 2020 Sevak Soghoyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AraratOrNot.
+FOUNDATION_EXPORT double AraratOrNotVersionNumber;
+
+//! Project version string for AraratOrNot.
+FOUNDATION_EXPORT const unsigned char AraratOrNotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AraratOrNot/PublicHeader.h>
+
+
